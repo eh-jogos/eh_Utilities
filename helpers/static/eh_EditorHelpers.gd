9ce9ec40838ba1f5317a2e2023fb2e64ccd0d5a2
Daniel Queiroz Porto <danielvandalk@gmail.com>
2021-07-02 09:14:58 -0300

    Add signal and error helpers to EditorHelpers
    I added them to help me deal with editor warnings withous simply
    ignoring or disabling them

--- helpers/static/eh_EditorHelpers.gd
+++ helpers/static/eh_EditorHelpers.gd
@@ -41,6 +41,39 @@ static func is_editor() -> bool:
 static func has_editor() -> bool:
 	return OS.has_feature("editor")
 
+
+static func connect_between(
+		from: Object, p_signal: String, 
+		to: Object, p_callback: String, binds: = [], 
+		type: = 0
+) -> void:
+	var is_alright: = OK
+	if not from.is_connected(p_signal, to, p_callback):
+		is_alright = from.connect(p_signal, to, p_callback, binds, type)
+	if is_alright != OK:
+		var msg = "failed to connect %s from %s to %s in %s"%[p_signal, from, p_callback, to]
+		not_alright_error(from, msg, from.get_script())
+
+
+static func disconnect_between(
+		from: Object, p_signal: String, to: Object, p_callback: String
+) -> void:
+	if from.is_connected(p_signal, to, p_callback):
+		from.disconnect(p_signal, to, p_callback)
+
+
+static func not_alright_error(who:Object, why: String, where: Script) -> void:
+	var who_name: String = ""
+	if who is Node:
+		who_name = who.name
+	elif who is Resource:
+		who_name = who.resource_path
+	elif who.get_script() != null:
+		who_name = who.get_script().resource_path
+	else:
+		who_name = str(who)
+	push_error("%s is not alright and %s at %s"%[who_name, why, where.resource_path])
+
 ### -----------------------------------------------------------------------------------------------
 
 
